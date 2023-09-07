358f3b07983f24eb7d0106e3eb275a090a7fe798
Charity Mutembei <charity.softwaredev@gmail.com>
2023-09-07 17:38:12 +0300

    edited: 2-do_deploy_web_static.py created:  Pushed at 2023-09-07 17:...

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,17 +3,38 @@
 Fabric script that distributes an archive to your web servers
 """
 
-from fabric.api import env, put, run, prompt
+from fabric.api import env, put, run, local
+from datetime import datetime
 import os
 
-# Prompt for the list of hostnames (comma-separated) and the username
-hostnames = prompt("Enter a comma to separate (e.g., host1,host2): ")
-username = prompt("Enter the username: ")
+env.hosts = ["100.25.180.249", "54.196.30.13"]
+env.user = "ubuntu"
 
-# Set the list of hosts and the username in the env dictionary
-env.hosts = [hostname.strip() for hostname in hostnames.split(",")]
-env.user = username
+def do_pack():
+    """
+    Compresses the contents of the web_static folder into a .tgz archive.
+    """
+    try:
+        # Create the versions directory if it doesn't exist
+        if not os.path.exists("versions"):
+            os.makedirs("versions")
 
+        # Get the current date and time
+        now = datetime.now()
+
+        # Format the date and time as a string
+        date_time_str = now.strftime("%Y%m%d%H%M%S")
+
+        # Create the archive filename
+        archive_name = "web_static_{}.tgz".format(date_time_str)
+
+        # Compress the web_static folder into the archive
+        local("tar -cvzf versions/{} web_static".format(archive_name))
+
+        # Return the archive path if successful
+        return "versions/{}".format(archive_name)
+    except Exception:
+        return None
 
 def do_deploy(archive_path):
     """
@@ -39,13 +60,7 @@ def do_deploy(archive_path):
         run("mkdir -p {}{}/".format(remote_release_path, archive_folder_name))
 
         # Uncompress the archive to the folder on the web servers
-        run("tar -xzf {}{} -C {}{}/".format
-            (
-                remote_tmp_path,
-                archive_name,
-                remote_release_path,
-                archive_folder_name
-                ))
+        run("tar -xzf {}{} -C {}{}/".format(remote_tmp_path, archive_name, remote_release_path, archive_folder_name))
 
         # Delete the archive from the web servers
         run("rm -f {}{}".format(remote_tmp_path, archive_name))
@@ -54,10 +69,7 @@ def do_deploy(archive_path):
         run("rm -f /data/web_static/current")
 
         # Create a new symbolic link linked to the new version
-        run("ln -s {}{}/ /data/web_static/current".format(
-            remote_release_path,
-            archive_folder_name
-            ))
+        run("ln -s {}{}/ /data/web_static/current".format(remote_release_path, archive_folder_name))
 
         return True
 
