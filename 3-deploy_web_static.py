8c8091df26ce1d7f5206cbab5d6f6020dcb93a28
Charity Mutembei <charity.softwaredev@gmail.com>
2023-09-07 23:25:48 +0300

    edited: 3-deploy_web_static.py Pushed at 2023-09-07 23:25:48

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -39,40 +39,28 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """Distribute an archive to web servers
     """
-    if not os.path.exists(archive_path):
-        return False
-
-    try:
-        # Upload the archive to /tmp/ directory on the web server
+        Distribute archive.
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
-
-        # Extract the archive to the web_static/releases/ directory
-        archive_filename = os.path.basename(archive_path)
-        folder_name = "/data/web_static/releases/{}".format(
-            archive_filename[:-4])
-        run("sudo mkdir -p {}".format(folder_name))
-        run(" sudo tar -xzf /tmp/{} -C {}".format(
-            archive_filename, folder_name))
-
-        # Delete the uploaded archive from /tmp/
-        run(" sudo rm /tmp/{}".format(archive_filename))
-
-        # Move the contents of the extracted folder to the parent directory
-        run("sudo mv {}/web_static/* {}".format(folder_name, folder_name))
-
-        # Remove the symbolic link /data/web_static/current
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
-
-        # Create a new symbolic link /data/web_static/current
-        run("sudo ln -s {} /data/web_static/current".format(folder_name))
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
         print("New version deployed!")
         return True
-    except Exception:
-        return False
 
+    return False
 
 def deploy():
     """
