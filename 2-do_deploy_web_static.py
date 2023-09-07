db98bbf76d9fce4428f30e5b221d0197022be715
Charity Mutembei <charity.softwaredev@gmail.com>
2023-09-07 17:30:46 +0300

    edited: 2-do_deploy_web_static.py Pushed at 2023-09-07 17:30:46

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,57 +1,65 @@
 #!/usr/bin/python3
-"""compress and send to server
 """
-from fabric.api import local, env, run, put
-from datetime import datetime
+Fabric script that distributes an archive to your web servers
+"""
+
+from fabric.api import env, put, run, prompt
 import os
-env.hosts = ['35.231.156.161', '34.73.64.44']
-env.user = 'ubuntu'
 
+# Prompt for the list of hostnames (comma-separated) and the username
+hostnames = prompt("Enter a comma to separate (e.g., host1,host2): ")
+username = prompt("Enter the username: ")
 
-def do_pack():
-    """compress webstatic in a tgz
-    the tgz created will be put in folder versions
-    """
-    if not os.path.exists("versions"):
-        local("mkdir versions")
-    now = datetime.now()
-    name = "versions/web_static_{}.tgz".format(
-        now.strftime("%Y%m%d%H%M%S")
-    )
-    cmd = "tar -cvzf {} {}".format(name, "web_static")
-    result = local(cmd)
-    if not result.failed:
-        return name
+# Set the list of hosts and the username in the env dictionary
+env.hosts = [hostname.strip() for hostname in hostnames.split(",")]
+env.user = username
 
 
 def do_deploy(archive_path):
-    """send archive to server
-    Arguments:
-        archive_path: the archive tgz file to send
     """
-    if not archive_path or not os.path.exists(archive_path):
+    Distribute an archive to web servers
+    """
+    if not os.path.exists(archive_path):
         return False
-    put(archive_path, '/tmp')
-    ar_name = archive_path[archive_path.find("/") + 1: -4]
+
     try:
-        run('mkdir -p /data/web_static/releases/{}/'.format(ar_name))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(
-                ar_name, ar_name
-        ))
-        run('rm /tmp/{}.tgz'.format(ar_name))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(
-                ar_name, ar_name
-        ))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(
-            ar_name
-        ))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current'.format(
-            ar_name
-        ))
-        print("New version deployed!")
+        # Get the base name of the archive
+        archive_name = os.path.basename(archive_path)
+
+        # Define the remote paths
+        remote_tmp_path = "/tmp/"
+        remote_archive_path = remote_tmp_path + archive_name
+        remote_release_path = "/data/web_static/releases/"
+        archive_folder_name = archive_name[:-4]
+
+        # Upload the archive to the /tmp/ directory of the web servers
+        put(archive_path, remote_tmp_path)
+
+        # Create the directory for the new version
+        run("mkdir -p {}{}/".format(remote_release_path, archive_folder_name))
+
+        # Uncompress the archive to the folder on the web servers
+        run("tar -xzf {}{} -C {}{}/".format
+            (
+                remote_tmp_path,
+                archive_name,
+                remote_release_path,
+                archive_folder_name
+                ))
+
+        # Delete the archive from the web servers
+        run("rm -f {}{}".format(remote_tmp_path, archive_name))
+
+        # Delete the current symbolic link
+        run("rm -f /data/web_static/current")
+
+        # Create a new symbolic link linked to the new version
+        run("ln -s {}{}/ /data/web_static/current".format(
+            remote_release_path,
+            archive_folder_name
+            ))
+
         return True
-    except:
+
+    except Exception:
         return False
