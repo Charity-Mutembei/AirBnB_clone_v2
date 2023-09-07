289efd845fee5d5d4b4a068ae958b7af14f1aba1
Charity Mutembei <charity.softwaredev@gmail.com>
2023-09-07 21:40:26 +0300

    edited: 2-do_deploy_web_static.py Pushed at 2023-09-07 21:40:26

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,33 +28,35 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """send archive to server
-    Arguments:
-        archive_path: the archive tgz file to send
+    """Distribute an archive to web servers
     """
-    if not archive_path or not os.path.exists(archive_path):
+    if not os.path.exists(archive_path):
         return False
-    put(archive_path, '/tmp')
-    ar_name = archive_path[archive_path.find("/") + 1: -4]
+
     try:
-        run('mkdir -p /data/web_static/releases/{}/'.format(ar_name))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(
-                ar_name, ar_name
-        ))
-        run('rm /tmp/{}.tgz'.format(ar_name))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(
-                ar_name, ar_name
-        ))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(
-            ar_name
-        ))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current'.format(
-            ar_name
-        ))
-        print("New version deployed!")
+        # Upload the archive to /tmp/ directory on the web server
+        put(archive_path, "/tmp/")
+
+        # Extract the archive to the web_static/releases/ directory
+        archive_filename = os.path.basename(archive_path)
+        folder_name = "/data/web_static/releases/{}".format(
+            archive_filename[:-4])
+        run("mkdir -p {}".format(folder_name))
+        run("tar -xzf /tmp/{} -C {}".format(archive_filename, folder_name))
+
+        # Delete the uploaded archive from /tmp/
+        run("rm /tmp/{}".format(archive_filename))
+
+        # Move the contents of the extracted folder to the parent directory
+        run("mv {}/web_static/* {}".format(folder_name, folder_name))
+
+        # Remove the symbolic link /data/web_static/current
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link /data/web_static/current
+        run("ln -s {} /data/web_static/current".format(folder_name))
+
         return True
-    except Exception:
+    except Exception as e:
+        print(e)
         return False
