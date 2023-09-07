19779f323f3cf80727a4767bec03efd9910dfcd5
Charity Mutembei <charity.softwaredev@gmail.com>
2023-09-08 00:31:47 +0300

    edited: 101-setup_web_static.pp Pushed at 2023-09-08 00:31:47

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,58 +1,57 @@
-# Update the package list and install Nginx
-package { 'nginx':
-  ensure => installed,
+exec { 'update_system':
+  command => '/usr/bin/env apt-get update -y',
 }
 
-# Create necessary directories
-file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/releases/test', '/data/web_static/shared']:
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { '/data':
   ensure => 'directory',
 }
 
-# Create an HTML file for the test page
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  content => '<html>
+file { '/data/web_static':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/releases':
+  ensure => 'directory',
+}
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory',
+}
+
+$index_html_content = '
+<html>
 <head>
 </head>
 <body>
-  Holberton School
+  <p>Nginx server test</p>
 </body>
-</html>',
+</html>
+'
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => $index_html_content,
 }
 
-# Create a symbolic link to the test release
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test',
-  force  => true,
 }
 
-# Set ownership to ubuntu:ubuntu
-file { '/data':
-  owner => 'ubuntu',
-  group => 'ubuntu',
-  recurse => true,
+exec { 'chown_data':
+  command => 'chown -R ubuntu:ubuntu /data/',
+  path    => '/usr/bin:/usr/local/bin:/bin',
 }
 
-# Configure Nginx to serve the content
-file { '/etc/nginx/sites-available/web_static':
-  ensure  => 'file',
-  content => "server {
-    location /hbnb_static/ {
-        alias /data/web_static/current/;
-    }
-}",
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => template('your_module/nginx_config.erb'), # You need to create a template for the nginx configuration
 }
 
-# Create a symbolic link to enable the site
-file { '/etc/nginx/sites-enabled/web_static':
-  ensure => 'link',
-  target => '/etc/nginx/sites-available/web_static',
-}
-
-# Restart Nginx for configuration changes
 service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/web_static'],
+  ensure => 'running',
 }
