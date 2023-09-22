a1fca358a6229c5c5bdd55861cdb5eaf793a7a92
Charity Mutembei <charity.softwaredev@gmail.com>
2023-09-23 02:55:30 +0300

    edited: 5-number_template.py Pushed at 2023-09-23 02:55:30

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -7,42 +7,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def text_display(text):
-    formatted_text = text.replace('_', ' ')
-    return 'C {}'.format(formatted_text)
-
-
-@app.route('/python', strict_slashes=False)
-def python_without_text():
-    return 'Python is cool'
-
-
-@app.route('/python/<text>', strict_slashes=False)
-def python_with_text(text):
-    formatted_text_two = text.replace('_', ' ')
-    return 'Python {}'.format(formatted_text_two)
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def text_is_a_number(n):
-    return '{} is a number'.format(n)
-
-
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    if isinstance(n, int):
-        return render_template('5-number.html', number=n)
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == '__main__':
