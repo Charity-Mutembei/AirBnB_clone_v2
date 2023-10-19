700a50c1b4750c186722dc6c0308f29adb3139d1
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-10-19 10:21:58 +0000

    check

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,14 +3,28 @@
 this is a script that starts a Flask web application
 """
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
 
+def find_available_port(port):
+    while True:
+        try:
+            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server.bind(('0.0.0.0', port))
+            server.close()
+            return port
+        except OSError:
+            port += 1
+
+
+port = 5000
 if __name__ == '__main__':
+    port = find_available_port(port)
     app.run(host='0.0.0.0', port=5000)
